7430910983befcd3ce538df231d85cab00658cab
jrichardchemistry <jrichardquimica@gmail.com>
2020-06-27 10:20:16 -0300

    FG code implemented for a smiles format

--- pyCheckmol/pycheckmol.py
+++ pyCheckmol/pycheckmol.py
@@ -1,20 +1,67 @@
 import subprocess
+import os
 import pandas as pd
+from tempfile import NamedTemporaryFile
 
 class CheckMOl:
     def __init__(self):
         self.information = ''
 
-    def functionalGroups(self, file = '', justFGcode=True, returnDataframe=False):
+    def functionalGroupSmiles(self, file = '', generate3D=False,justFGcode=True, returnDataframe=True, deleteTMP=True):
         """
         This funtion returns the Functional groups (FG) information. Each FG is
         labeled with a code. Altogether there are 204 FG labeled. The table with
-        this information can be viewed at .......
+        this information can be viewed at 
+        https://github.com/jeffrichardchemistry/pyCheckmol/blob/master/examples/fgtable.pdf
         
         Arguments
         ---------------------
         file
-            Path to file, must be: .sdf, .mol, .mol2, .smiles.
+            Path to file, must be a smiles: .smiles or .smi
+        generate3D
+            If true the smiles will be converted into a sdf with 3D coordinates.
+            Openbabel run in backend. If False will be converted with 2D coordinates.
+        justFGcode
+            if True return just code of FG, If False return the FG's code,
+            number of atoms with these code and each atoms label as a dataframe
+            or dict.
+        returnDataframe
+            Use only justFGcode=False. If returnDataframe=False the result is a
+            dictionary, if True result is a dataframe.
+        deleteTMP
+            If True the temporary file will be deleted.
+        """
+        tmp_file = [i for i,x in enumerate(file) if x == '/']
+        tmp_file = file[:tmp_file[-1] + 1]
+        if generate3D:
+            smi2sdf = subprocess.getoutput('babel {} {}tmp.sdf --gen3D'.format(file, tmp_file))
+            fg = CheckMOl.functionalGroups(self, file=tmp_file+'tmp.sdf', justFGcode=justFGcode, returnDataframe=returnDataframe)
+            if deleteTMP:
+                os.remove(tmp_file+'tmp.sdf')
+            else:
+                pass
+            return fg
+        else:
+            smi2sdf = subprocess.getoutput('babel {} {}tmp.sdf --gen2D'.format(file, tmp_file))
+            fg = CheckMOl.functionalGroups(self, file=tmp_file+'tmp.sdf', justFGcode=justFGcode, returnDataframe=returnDataframe)
+            if deleteTMP:
+                os.remove(tmp_file+'tmp.sdf')
+            else:
+                pass
+            return fg
+
+
+    def functionalGroups(self, file = '', justFGcode=True, returnDataframe=True):
+        """
+        This funtion returns the Functional groups (FG) information. Each FG is
+        labeled with a code. Altogether there are 204 FG labeled. The table with
+        this information can be viewed at 
+        https://github.com/jeffrichardchemistry/pyCheckmol/blob/master/examples/fgtable.pdf
+        
+        Arguments
+        ---------------------
+        file
+            Path to file, must be: .sdf, .mol, .mol2
         justFGcode
             if True return just code of FG, If False return the FG's code,
             number of atoms with these code and each atoms label as a dataframe
@@ -37,11 +84,13 @@ class CheckMOl:
                 return getdf.to_dict('list')
         
 
-if __name__ == '__main__':
-    file = 'examples/mol.sdf'
-
+""" if __name__ == '__main__':
+    file = '/dados/programas/checkmol/mole3.sdf'
+    filesmi = '/dados/programas/checkmol/mole2.smiles'
     cm = CheckMOl()
-    get = cm.functionalGroups(file, justFGcode=True, returnDataframe=True)
-    print(get)
-    
+    #get = cm.functionalGroups(file, justFGcode=True, returnDataframe=False)
+    #print(get)
+
+    smi = cm.functionalGroupSmiles(file=filesmi, generate3D=False, justFGcode=True, returnDataframe=True,deleteTMP=True)
+    print(smi) """
 
